e7d32e2252ca90f0ac50186aa4327974ea0a3b1c
Sebastian Espiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-17 21:49:47 -0500

    feat: apply initial migrations and register models in django admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Author, AuthorProfile, Category, Publisher, Book, Publication
 
-# Register your models here.
+
+class PublicationInline(admin.TabularInline):
+    model = Publication
+    extra = 1
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    inlines = [PublicationInline]
+
+
+admin.site.register(Author)
+admin.site.register(AuthorProfile)
+admin.site.register(Category)
+admin.site.register(Publisher)
+admin.site.register(Publication)
